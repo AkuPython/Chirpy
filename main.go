11b82596ee4a21471e782b25be189c55bd10ff39
AkuPython <12842971+AkuPython@users.noreply.github.com>
2025-05-13 04:17:08 +0930

    add file serve on / and add assets/logo

--- main.go
+++ main.go
@@ -8,15 +8,18 @@ import (
 
 func main()  {
 	const port = "8080"
+	const rootPath = "."
 	
 	mux := http.NewServeMux()
-	
+	mux.Handle("/", http.FileServer(http.Dir(rootPath)))
+
 	srv := &http.Server{
 		Addr: ":" + port,
 		Handler: mux,
 	}
+	
 
-	log.Printf("Serving on port: %s\n", port)
+	log.Printf("Serving files from: '%s' on port: %s\n", rootPath, port)
 	log.Fatal(srv.ListenAndServe())
 
 }
